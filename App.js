5b68d69a6e4072dd007a22edea44067031d0c6fc
Paras <soodparassood4@gmail.com>
2020-08-12 14:06:57 +0530

    Background image

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default function App() {
   
   if(firebase.apps.length === 0){
     var firebaseConfig = {
+      //Firebase Api
       //Firebase Config
     };
     // Initialize Firebase
